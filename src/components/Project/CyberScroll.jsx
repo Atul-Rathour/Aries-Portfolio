323fa357e0aca6c607c413d17a485e63670439f6
Atul-Rathour <your-email@example.com>
2024-10-13 18:37:23 +0530

    fixing the project section

--- src/components/Project/CyberScroll.jsx
+++ src/components/Project/CyberScroll.jsx
@@ -68,7 +68,7 @@ const CyberScroll = ({ scrollRef }) => {
   if (isLoading) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+          <p>loading...</p>
       </div>
     );
   }
