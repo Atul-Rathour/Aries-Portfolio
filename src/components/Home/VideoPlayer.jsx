cc763828d708adf6b6c642e52b45f7a110d14398
Atul-Rathour <your-email@example.com>
2024-10-16 19:09:51 +0530

    adding the cover on intro

--- src/components/Home/VideoPlayer.jsx
+++ src/components/Home/VideoPlayer.jsx
@@ -18,12 +18,20 @@ const VideoPlayer = ({ onVideoEnd }) => {
     };
   }, [onVideoEnd]);
 
+
+  const cover = useRef()
   return (
     <div className="video-container absolute w-[100vw] h-[100vh] z-[100]">
-      <video ref={videoRef} autoPlay muted className="w-[100%] h-[100%] object-cover " >
+      <video onLoadedData={() => {
+        cover.current.style.opacity = 0;
+      }} ref={videoRef} autoPlay muted className="w-[100%] h-[100%] object-cover " >
         <source src={Vid} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      <div ref={cover} className="w-[100vw] h-[100vh] absolute z-[2] bg-[#fff] " >
+        Waiting
+      </div>
     </div>
   );
 };
